d64f7814575a815b798eed8c2c6da8329ad6faa2
Renan Alvares <renan.s.alvares@gmail.com>
2026-07-21 20:28:02 -0300

    commit inicial

---
+++ blog/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import get_object_or_404, render
+from blog.models import Postagem
+from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect
+
+def home(request):
+    postagens = Postagem.objects.all().order_by("-data_criacao")
+
+    return render(request, 'home.html', {'postagens':postagens})
+
+def detalhe_post(request, pk):
+    postagem = Postagem.objects.get(pk=pk)
+    return render(request, 'detalhe_postagem.html', {'postagem':postagem})
+
+
+def adicionar_postagem(request):
+    if request.method == 'POST':
+        #salvar postagem
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            postagem = form.save(commit=False)
+            postagem.autor = request.user
+            postagem.data_publicacao = timezone.now()
+            postagem.save()
+            return redirect('home')
+
+    else:
+        #primeiro acesso
+        form = PostForm()
+
+    return render(request, 'editar_postagem.html', {'form':form})
+
+
+def edit_postagem(request,pk):
+    postagem = get_object_or_404(Postagem, pk=pk)
+
+    if request.method == 'POST':
+        #salvar
+        form = PostForm(request.POST, instance=postagem)
+
+        if form.is_valid():
+
+            postagem = form.save(commit=False)
+            postagem.autor = request.user
+            postagem.data_publicacao = timezone.now()
+            postagem.save()
+            return redirect('home')
+    
+    else:
+        #primeiro acesso
+        form = PostForm(instance=postagem)
+
+    return render(request, 'editar_postagem.html', {'form':form})
+
